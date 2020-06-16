a44f51117f84aa2e4801dc498f9e40d3e9061ce6
YutaGoto <you.goto.510@gmail.com>
2020-06-16 17:56:40 +0900

    init commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use rand::Rng;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let vs = vec!["a", "b", "c", "b", "c", "d"];
+    let num = rng.gen_range(1, 170);
+    let q = vs[rng.gen_range(0, 5)];
+    println!("{}", format!("Let's challenge https://atcoder.jp/contests/abc{}/tasks/abc{}_{}", num, num, q));
+}
