80c65c9e621f2dbe978d1d7f10a9eb0ca8e51fbf
YutaGoto <you.goto.510@gmail.com>
2020-06-17 00:13:39 +0900

    fix contest num

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 fn main() {
     let mut rng = rand::thread_rng();
     let vs = vec!["a", "b", "c", "b", "c", "d"];
-    let num = rng.gen_range(1, 170);
+    let num = rng.gen_range(42, 170);
     let q = vs[rng.gen_range(0, 5)];
-    println!("{}", format!("Lets challenge https://atcoder.jp/contests/abc{}/tasks/abc{}_{}", num, num, q));
+    println!("{}", format!("Lets challenge https://atcoder.jp/contests/abc{0: >03}/tasks/abc{1: >03}_{2:}", num, num, q));
 }
