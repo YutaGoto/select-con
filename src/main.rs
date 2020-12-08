3e75bfbcd84b2d8df9c5d686895e52a10538da72
YutaGoto <you.goto.510@gmail.com>
2020-12-08 11:49:47 +0900

    add d plobrem

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use rand::Rng;
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let vs = vec!["a", "b", "c", "b", "c", "d"];
-    let num = rng.gen_range(42, 179);
-    let q = vs[rng.gen_range(0, 5)];
+    let vs = vec!["a", "b", "c", "b", "c", "d", "c", "d"];
+    let num = rng.gen_range(42, 184);
+    let q = vs[rng.gen_range(0, 7)];
     println!("{}", format!("Lets challenge https://atcoder.jp/contests/abc{0: >03}/tasks/abc{1: >03}_{2:}", num, num, q));
 }
