2696f1daa07fcee05ae69a2a3e13925738de9f2f
Yuta Goto <you.goto.510@gmail.com>
2021-06-21 01:15:27 +0900

    up to 206

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 fn main() {
     let mut rng = rand::thread_rng();
     let vs = vec!["a", "b", "c", "b", "c", "d", "c", "d"];
-    let num = rng.gen_range(42, 184);
+    let num = rng.gen_range(42, 206);
     let q = vs[rng.gen_range(0, 7)];
     println!("{}", format!("Lets challenge https://atcoder.jp/contests/abc{0: >03}/tasks/abc{1: >03}_{2:}", num, num, q));
 }
