867190edc2445dd75b521d70007f79e205c92098
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-05-08 23:08:00 +0900

    :memo: fix output text

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ fn main() {
     let vs = vec!["a", "b", "c", "d"];
     let num = rng.gen_range(42, 252);
     let q = vs[rng.gen_range(0, 3)];
-    println!("{}", format!("Lets challenge https://atcoder.jp/contests/abc{0: >03}/tasks/abc{1: >03}_{2:}", num, num, q));
+    println!("{}", format!("https://atcoder.jp/contests/abc{0: >03}/tasks/abc{1: >03}_{2:}", num, num, q));
 }
