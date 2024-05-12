3c358a6b1e263be3dfea3ede9262044eb860a972
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-05-12 21:19:53 +0900

    :boom: fix gen_range args

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 fn main() {
     let mut rng = rand::thread_rng();
     let vs = vec!["a", "b", "c", "d"];
-    let num = rng.gen_range(42, 353);
-    let q = vs[rng.gen_range(0, 3)];
+    let num = rng.gen_range(42..=353);
+    let q = vs[rng.gen_range(0..=3)];
     println!("{}", format!("https://atcoder.jp/contests/abc{0: >03}/tasks/abc{1: >03}_{2:}", num, num, q));
 }
